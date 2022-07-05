1e4a2633755490b3335a474012b0f6e95d9af09d
Daniel Au <daniel.au123@hotmail.com>
2022-07-05 18:25:22 -0400

    updating scarping.py (10.3.6)

--- scraping.py
+++ scraping.py
@@ -1,15 +1,15 @@
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
+# Visit the Mars news site
+url = 'https://redplanetscience.com/'
 browser.visit(url)
 
 # Optional delay for loading the page
@@ -20,9 +20,10 @@ html = browser.html
 news_soup = soup(html, 'html.parser')
 
 slide_elem = news_soup.select_one('div.list_text')
+
 slide_elem.find('div', class_='content_title')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
+# Use the parent element to find the first a tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
@@ -30,13 +31,13 @@ news_title
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
 
-# ### Featured Images
+# ## JPL Space Images Featured Image
 
 # Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
 
-# Find and click the full image button, 2nd button in website so [1]
+# Find and click the full image button
 full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
@@ -44,18 +45,21 @@ full_image_elem.click()
 html = browser.html
 img_soup = soup(html, 'html.parser')
 
-# Find the relative image url
+# find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
 
-# Use the base URL to create an absolute URL
+# Use the base url to create an absolute url
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
 
-# ### Mars Facts Table
+# ## Mars Facts
+
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
+df.head()
+
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
 df
 
 df.to_html()
